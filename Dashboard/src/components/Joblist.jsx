d7d0e430b92cfbe1c0cd596641c36ca6c347cfe1
Javakar N <123374417+Javakar4@users.noreply.github.com>
2025-06-09 19:16:33 +0530

    Update Joblist.jsx

--- Dashboard/src/components/Joblist.jsx
+++ Dashboard/src/components/Joblist.jsx
@@ -40,7 +40,7 @@ const JobList = ({ filters }) => {
       {filteredJobs.map((job) => (
         <JobCard
           key={job._id}
-          logo={`http://localhost:4000/uploads/${job.logo}`}
+          logo={`https://job-portal-backend-ajr3.onrender.com/uploads/${job.logo}`}
           title={job.title}
           time={formatDistanceToNow(new Date(job.createdAt), { addSuffix: true })}
           experience={job.type}
