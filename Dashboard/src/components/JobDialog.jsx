5610ecabbc61499d9b333c0a3aa0a35699896a48
Javakar N <123374417+Javakar4@users.noreply.github.com>
2025-06-09 18:11:43 +0530

    Update JobDialog.jsx

--- Dashboard/src/components/JobDialog.jsx
+++ Dashboard/src/components/JobDialog.jsx
@@ -85,7 +85,7 @@ const JobDialog = ({ open, handleClose }) => {
         formData.append(key, jobDetails[key]);
       }
 
-      const response = await fetch('http://localhost:4000/api/jobs', {
+      const response = await fetch('https://job-portal-backend-ajr3.onrender.com/api/jobs', {
         method: 'POST',
         body: formData,
       });
